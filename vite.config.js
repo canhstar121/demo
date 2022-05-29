942a85695ffc9c4c186bd04eb8d5a76b64ac106f
canhnd20613 <canhnd2013>
2022-05-29 15:13:22 +0800

    tạo các page

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detailProduct: resolve(__dirname, 'deltail.html'),
+        products: resolve(__dirname, 'products.html'),
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
+  }
+})
